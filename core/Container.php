ffaf819a1e05d787eb628fdf390e1d70ea23d8b8
AntonyDamico <antonydamico29@gmail.com>
2019-08-25 14:41:16 +0200

    dependency container

---
+++ core/Container.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+    private static $container = [];
+
+
+    public static function bind($key, $value)
+    {
+        static::$container[$key] = $value;
+    }
+
+    
+    public static function get($key)
+    {
+        return static::$container[$key];
+    }
+}
