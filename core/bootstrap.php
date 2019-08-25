efa5bb01ae280fb2852557770ad4abf9950d095d
AntonyDamico <antonydamico29@gmail.com>
2019-08-25 15:13:51 +0200

    database connection

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,5 +2,10 @@
 
 use Dotenv\Dotenv;
 
+use Core\Container;
+use Core\Database\Connection;
+
 $dotenv = new DotEnv(dirname(__DIR__));
 $dotenv->load();
+
+Container::bind('database', (new Connection)->getConnection());
