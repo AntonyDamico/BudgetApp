18000a400a674a7a719ae9fba3c1cad3d9bf7653
AntonyDamico <antonydamico29@gmail.com>
2019-08-26 23:16:45 +0200

    adding more rules

--- app/validators/BudgetDataValidator.php
+++ app/validators/BudgetDataValidator.php
@@ -2,12 +2,23 @@
 
 namespace App\Validators;
 
-class BudgetDataValidator {
+class BudgetDataValidator
+{
 
-    public static function validate($post) {
-        if(!isset($post['income'])) die("income missing\n");
-        if(!isset($post['savings_perc'])) die("saving percentage missing\n");
-        if($post['income'] < 0) die("income cannot be less than 0\n");
-        if($post['savings_perc'] < 0) die("savings_perc cannot be less than 0\n");        
+    public static function validate($post)
+    {
+        if (!isset($post['income'])) die("income missing\n");
+        if (!isset($post['savings_perc'])) die("saving percentage missing\n");
+        if(!is_numeric($post['income']) || !is_numeric($post['savings_perc']))
+            die("The values have to be numeric\n");
+
+        $income = intval($post['income']);
+        $savingsPerc = intval($post['savings_perc']);
+        
+        if ($income < 0) die("income cannot be less than 0\n");
+        if ($savingsPerc < 0) die("savings_perc cannot be less than 0\n");
+        if ($savingsPerc > 100) die("savings_perc cannot be more than 100\n");
+
+        return [$income, $savingsPerc];
     }
 }
