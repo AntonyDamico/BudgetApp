975ec6801c5523d1ae18b4f1e13edc955a787f29
AntonyDamico <antonydamico29@gmail.com>
2019-08-26 22:00:39 +0200

    updating savings ammount

--- app/controllers/BudgetDataController.php
+++ app/controllers/BudgetDataController.php
@@ -19,7 +19,13 @@ class BudgetDataController
             'income' => $_POST['income'],
             'savings_perc' => $_POST['savings_perc']
         ];
+
+        $data['savings'] = $data['income'] * $data['savings_perc'] * 0.01;
+
         BudgetDataService::update($data);
-        echo json_encode(['jaja']);
+        echo json_encode([
+            'status' => 200,
+            'response' => 'budget updated'
+        ]);
     }
 }
