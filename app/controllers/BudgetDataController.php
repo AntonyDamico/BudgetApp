af4eccd2685355e8b1bbaee686a02b5d28a56844
AntonyDamico <antonydamico29@gmail.com>
2019-08-26 22:16:10 +0200

    data validation

--- app/controllers/BudgetDataController.php
+++ app/controllers/BudgetDataController.php
@@ -15,6 +15,13 @@ class BudgetDataController
 
     public function update()
     {
+        if (
+            !isset($_POST['income']) || !isset($_POST['savings_perc'])
+            || $_POST['income'] < 0 || $_POST['savings_perc'] < 0
+        ) {
+            die("error with post parameters\n");
+        }
+
         $data = [
             'income' => $_POST['income'],
             'savings_perc' => $_POST['savings_perc']
