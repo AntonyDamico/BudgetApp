d21c33f0690442f0e6b82c617d0425ee94aee6c8
AntonyDamico <antonydamico29@gmail.com>
2019-08-28 21:02:49 +0200

    inserting connection in container

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -9,6 +9,8 @@ use Core\Database\QueryBuilder;
 $dotenv = new DotEnv(dirname(__DIR__));
 $dotenv->load();
 
+Container::bind('connection', (new Connection)->getConnection());
+
 Container::bind('database', new QueryBuilder(
-    (new Connection)->getConnection()
+    Container::get('connection')
 ));
